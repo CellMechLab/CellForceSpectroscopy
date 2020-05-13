42f12279a761eb2f94af7ee714d453f142cb850e
Massimo Vassalli <massimo.vassalli@cnr.it>
2020-05-13 23:08:23 +0100

    cleanup

--- segmentation.py
+++ segmentation.py
@@ -2,156 +2,184 @@
 
 import numpy as np
 
+
 class trait():
-    def __init__(self,  x,y):
+    def __init__(self, x, y,imin,imax):
         self.x = x
         self.y = y
+        self.imin=imin
+        self.imax=imax
         self.accept = True
         self.last = True
+        self.first = True
         self.pj = 'P'
+        self.height = None
 
-    def __sub__(s1,s2):
-        x1,y1 = s1.getPoints()
-        x2,y2 = s2.getPoints()
-        
-        if x1[0]>x2[0]:    
-            h = y2[1]-y1[0]
+    def __sub__(s1, s2):
+        x1, y1 = s1.getPoints()
+        x2, y2 = s2.getPoints()
+
+        if x1[0] > x2[0]:
+            h = y2[1] - y1[0]
         else:
-            h = y1[1]-y2[0]
+            h = y1[1] - y2[0]
         return h
 
+    def reject(self):
+        self.accept = False
+        self.first = False
+        self.last = False
+
     def val(self):
+        if len(self.y)<2:
+            return 0.0
         return np.average(self.y)
 
     def getmq(self):
+        if len(self.y)<2:
+            return 0.0,0.0
         return np.polyfit(self.x, self.y, 1)
 
-    def slope(self):
-        m,q = self.getmq()
-        return np.arctan(m)*180.0/np.pi
-            
+    def slope(self,angle=True):
+        if len(self.y)<2:
+            return 0.0
+        m, q = self.getmq()
+        if angle is True:
+            return np.arctan(m) * 180.0 / np.pi
+        else:
+            return m
+
     def alen(self):
-        return np.sqrt((self.x[-1]-self.x[0])**2+(self.y[-1]-self.y[0])**2)
-
+        if len(self.y)<2:
+            return 0.0
+        return np.sqrt((self.x[-1] - self.x[0])**2 + (self.y[-1] - self.y[0])**
+                       2)
     def hlen(self):
-        return np.abs(self.x[-1]-self.x[0])
-    
-    def getPoints(self,mode='lin',polyorder=2):
-        x = [self.x[0],self.x[-1]]
-        if mode=='flat':
-            y = [self.val(),self.val()]
-        elif mode=='lin':
-            m,q = self.getmq()
-            y = [m*self.x[0]+q,m*self.x[-1]+q]
-        elif mode=='poly':
-            p = np.polyfit(self.x, self.y, polyorder)
-            x = np.linspace(self.x[0],self.x[-1],num=50)
-            y = np.polyval(p,x)
-        return x,y
+        if len(self.y)<2:
+            return 0.0
+        return np.abs(self.x[-1] - self.x[0])
 
-    
-        
+    def getPoints(self, mode='lin', polyorder=2, extend=False):
+        x = [self.x[0], self.x[-1]]
+        if mode == 'flat':
+            y = [self.val(), self.val()]
+        elif mode == 'lin':
+            m, q = self.getmq()
+            if extend is True:
+                x = [0, self.x[-1]]
+            y = [m * x[0] + q, m * x[-1] + q]
+        elif mode == 'poly':
+            p = np.polyfit(self.x, self.y, polyorder)
+            if extend is True:
+                x = np.linspace(0.0, self.x[-1], num=50)
+            else:
+                x = np.linspace(self.x[0], self.x[-1], num=50)
+            y = np.polyval(p, x)
+        elif mode=='curve':
+            return self.x,self.y
+        return x, y
+
+
 import savitzky_golay as sg
+
 
 class segmentation():
     def __init__(self):
-        self.slope=45
-        self.filtorder=3
-        self.mainth=1.5
-        self.minlen=10
-        self.zmin=0.0
-        self.deltaF=5.0
-        self.window= 2.0
-        self.absth = None
-        self.abswin = None
-        self.delta = 3.0/5.0
+        self.slope = 45
+        self.filtorder = 3
+        self.mainth = 1.5
+        self.minlen = 10
+        self.zmin = 0.0
+        self.deltaF = 5.0
+        self.window = 2.0
+        self.delta = 3.0 / 5.0
         self.trorder = 1.0
 
-    def run(self,p):
-        #ifrom = np.argmax(p.f)
-        x = p.z#[ifrom:]
-        y = p.f#[ifrom:]
-        self.abswin = int( self.window * float(len(x))/1000.0)
-        y2 = sg.getSG(y,filtwidth=self.abswin,filtorder=self.filtorder,deriv=1)
-        y3 = sg.getSG(y,filtwidth=self.abswin*self.delta,filtorder=self.filtorder,deriv=1)
-        self.absth = np.std(y2-y3) * self.mainth
-        
-        return self.act(x,y)
+    def abswin(self, p):
+        awin = int(self.window * float(len(p.z)) / 1000.0)
+        if awin < 5.0:
+            return 5.0
+        return awin
 
-    def getStat(tr):
-        L=[]
-        P=[]
-        H=[]
-        n=0
-        pL = 0
-        pMin = 0
-        prev = None
-        for s in tr:
-            if s.accept:
-                if s.last:
-                    n=n+1
-                    if n>1:
-                        pL += s.len()
-                        L.append(pL)
-                        P.append(pMin)
-                        H.append(getH(s,prev))
-                        pL = 0
-                else:
-                    pL += s.len()
-                pMin = min(s.x)
-                prev=s
-        return n-1,L,P,H
+    def absth(self, p):
+        window = self.abswin(p)
+        y2 = sg.getSG(p.f, filtwidth=window, filtorder=self.filtorder, deriv=1)
+        y3 = sg.getSG(
+            p.f,
+            filtwidth=window * self.delta,
+            filtorder=self.filtorder,
+            deriv=1)
+        return np.std(y2 - y3) * self.mainth
 
-    def act(self,x,y):
-        """    
-        mainth: threshold of the first derivative to identify steps    
+    def run(self, p, debug=False):
+        """
+        mainth: threshold of the first derivative to identify steps
         vth length above witch a segment is considered a plateau
         filtwidth,filtorder: width and order of the SG filter
         plath: distance from the origin of the first plateaux
         """
         #identification of the steps by first derivative peaks
+        #x,y=p.z,p.f
+        derthresh = self.absth(p)
+        der = sg.getSG(p.f, self.abswin(p), self.filtorder, deriv=1)
+        segments = []
 
-        der = sg.getSG(y, self.abswin, self.filtorder, deriv=1)    
-        xi = np.where(der<self.absth)
-        
-        segments = []                 
-        bmleft = xi[0]-np.roll(xi[0],1)-1
-        bmright = np.roll(xi[0],-1)-xi[0]-1
-        bordermatrix = bmleft+bmright
-        
-        borders = np.where(bordermatrix!=0)[0]
-    
-        bmin = borders-np.roll(borders,1)
-        bmax = borders-np.roll(borders,-1)
-        btrue = borders[np.where(bmin+bmax != 0)]
-    
-    
-        iborders = xi[0][btrue]
-        np.append(0,iborders,len(der)-1)
-          
+        xi = np.arange(len(der))
+        nw = np.logical_and(der >= -derthresh, der <= derthresh)
+        q = np.zeros(len(xi))
+        q[nw] = 1
+        iborders = xi[np.logical_xor(q - np.roll(q, 1) == 1, q - np.roll(
+            q, -1) == 1)]
+        if -derthresh <= der[0] <= derthresh:
+            if len(iborders) > 0:
+                if iborders[0] != 0:
+                    iborders = np.append(0, iborders)
+            else:
+                iborders = np.append(0, iborders)
+        if -derthresh <= der[-1] <= derthresh:
+            if len(iborders) > 0:
+                if iborders[-1] != xi[-1]:
+                    iborders = np.append(iborders, xi[-1])
+        if debug is True:
+            return iborders
+
         ibinv = iborders[::-1]
         previous = None
-        for i in range(0,len(ibinv)-1,2):
-            xx = x[ibinv[i+1]:ibinv[i]]
-            yy = y[ibinv[i+1]:ibinv[i]]
-            segfound=trait(xx,yy)
-            
-            if self.zmin>0 and xx[0]-x[0]<self.zmin:
-                segfound.accept=False
-            elif self.zmin<0 and x[0]-xx[0]<self.zmin:
-                segfound.accept=False
+        #print('iborders {}'.format(iborders))
+        for i in range(0, len(ibinv) - 1, 2):
+            xx = p.z[ibinv[i + 1]:ibinv[i]]
+            yy = p.f[ibinv[i + 1]:ibinv[i]]
+            segfound = trait(xx, yy,ibinv[i + 1],ibinv[i])
+
+            if self.zmin > 0 and xx[0] - p.z[0] < self.zmin:
+                segfound.reject()
+            elif self.zmin < 0 and p.z[0] - xx[0] < self.zmin:
+                segfound.reject()
             else:
-                if segfound.hlen()<=self.minlen:
-                    segfound.accept=False
+                if np.abs(segfound.slope()) < self.slope:
+                    segfound.pj = 'P'
+                else:
+                    segfound.pj = 'J'
+                if segfound.hlen() <= self.minlen:
+                    segfound.reject()
                 else:
                     if previous != None:
-                        h = segfound-previous
+                        h = segfound - previous
+                        segfound.height = h
                         if np.abs(h) < self.deltaF:
-                            segfound.last = False
+                            segfound.last = True
+                            segfound.first = False
+                            previous.last = False
+                        #    previous = segfound
                         else:
-                            previous = segfound
-                    else:
+                            segfound.first = True
+                            segfound.last = True
                         previous = segfound
+                    else:
+                        segfound.first = True
+                        segfound.last = True
+                        previous = segfound
+
             segments.append(segfound)
-    
+
         return segments
--- sifork/segment.py
+++ sifork/segment.py
@@ -1,12 +1,14 @@
 import numpy as np
 from . import mvobject
+from scipy.integrate import trapz
 try:
     import savitzky_golay as sg
 except:
     from .. import savitzky_golay as sg
 
+
 class segment(mvobject.mvobject):
-    def __init__(self,x,y):   
+    def __init__(self, x=None, y=None):
         #internal units
         #  sensitivity nm/V
         #  k pN/nm
@@ -14,57 +16,284 @@ class segment(mvobject.mvobject):
         #  Z nm
         #  F pN
         #  directions are near | far | hold
-        defaults = {'direction':'far','speed':1.0,'k':1.0,'type':'Vconst'}
-        self.parseConfig(defaults,'Segment')
-        if len(x)==0:
-            return False
-         
-        if x[0]>x[-1]:
-            self.direction='near'
-            x.reverse()
-            y.reverse()
-            
+        defaults = {
+            'direction': 'far',
+            'speed': 0.0,
+            'k': 1.0,
+            'type': 'Vconst',
+            'duration': 0.0,
+            'setpoint': 0.0,
+            'zstart':0.0,
+            'zend':0.0
+        }
+        self.direction= 'far'
+        self.speed= 0.0
+        self.k= 1.0
+        self.type= 'Vconst'
+        self.duration= 0.0
+        self.setpoint= 0.0
+        self.zstart=0.0
+        self.zend=0.0
+        self.parseConfig(defaults, 'Segment')
         self.show = True
+        self.traits = []
+        self._x=[]
+        self._y=[]
+        self.z = np.array([])
+        self.f = np.array([])
+        if x is not None and y is not None:
+            self.setXY(x,y)
 
+    def setSpeed(self,speed=None):
+        if speed is None:
+            delta = np.abs(self.zstart-self.zend)
+            if self.duration != 0:
+                self.speed = delta/self.duration
+            else:
+                self.speed = 0.0
+        else:
+            self.speed=speed
+        
+    def setXY(self,x=None,y=None):
+        if x is None:
+            x,y = self._x,self._y
+        if len(x)==0 or len(y)==0:
+            return
+        if x[0] > x[-1]:
+            self.direction = 'near'
+            x.reverse()
+            y.reverse()        
         self.z = np.array(x)
-        self.f = np.array(y) 
+        self.f = np.array(y)
+        self._x,self._y=None,None
 
-    def getRelevant(self):
+    def getRelevant(self,val=0.0):
         for i in range(len(self.z)):
-            if self.z[i]>=0.0:
+            if self.z[i] >= val:
                 start = i
                 break
-        return self.z[start:],self.f[start:]
-            
+        return self.z[start:], self.f[start:]
+
     def FZtoFD(self):
         """
         Convert Force versus Displacement to Force versus Distance
         """
-        self.z=self.z-self.f/self.k
+        self.z = self.z - self.f / self.k
 
-    def getContactIndex(self, smooth=True):
+    def getContactIndex(self, smooth=True,val = 0.0):
         y = self.f
-        if smooth:
-            y = sg.getSG (self.f, len(self.f/100), 2, 0)
-        for i in range(len(y)-1):
-            if ( ( y[i]+1 > 0) and (y[i]<0) ):
-                return i+1
-        return 0
+        if smooth is True:
+            y = sg.getSG(self.f, len(self.f / 100), 2, 0)
+        if y[0]>=val:
+            return 0
+        for i in range(len(y) - 1):
+            if ((y[i] + 1 > val) and (y[i] < val)):
+                return i + 1
+        return 0    
 
-    def getArea(self):
+    def getNumbers(self):
+        nump = 0
+        numj = 0
+        numb = 0
+        for i in range(len(self.traits)):
+            t = self.traits[i]
+            if t.accept:
+                if t.pj == 'P':
+                    nump += 1
+                else:
+                    numj += 1
+            else:
+                numb += 1
+        return nump, numj, numb
+
+    def getStretchTo(self,toDistance):
+        from scipy import interpolate
+        x = self.z
+        y = self.f
+        x0,y0 = self.getOrigin()
+        rep = interpolate.splrep(x, y)
+        return interpolate.splint(x0, x0+toDistance, rep)
+
+    def getStretchArea(self,toJump=False):
+        x = self.z
+        y = self.f
+
+        istart = self.getOrigin(True)
+
+        if toJump is True:
+            iend = self.getNearFarPoint(True)
+        else:
+            iend = self.getJumpPoint(True)
+
+        if istart >= iend:
+            return 0
+        
+        from scipy.integrate import trapz
+        area = trapz(y[istart:iend], x[istart:iend])
+        return area
+
+    def getArea(self, reflatten=False, polyorder=2):
         """
-
         :rtype : float
         """
-        y = self.f
         x = self.z
+        y = self.f
+
+        istart = self.getOrigin(True)
+
+        if reflatten and len(self.traits) > 0:
+            p = np.polyfit(self.traits[0].x, self.traits[0].y, polyorder)
+            y = y - np.polyval(p, x)
 
         from scipy.integrate import trapz
-        i = self.getContactIndex(smooth=False)
-        return trapz(y[i:],x[i:])
+        return trapz(y[istart:], x[istart:])
+
+    def getMaxStep(self,filter=False,rngX=[-9999,99999],rngLen=[0,99999]):
+        if len(self.traits)==0:
+            return 0.0
+
+        data = []
+        j=0
+        pret = self.traits[0]
+        for t in self.traits:
+            if t.accept is True and j>0:
+                number = t-pret
+                pret = t
+                if filter is True:
+                    if (rngX[0] < min(t.x) < rngX[1]):
+                        if (rngLen[0] < t.alen() < rngLen[1]):
+                            data.append(number)
+                else:
+                    data.append(number)
+            j+=1
+        if len(data)==0:
+            return 0.0
+        return max(data)
+
+    def zShift(self,val):
+        self.traits = []
+        self.z -= val
+
+    def fShift(self,val):
+        self.traits = []
+        self.f -= val
+
+    def rescale(self,m):
+        self.traits = []
+        self.f /= m
+
+    def getOrigin(self,indexonly=False):
+        u = self.getUltimi()
+        if len(u)==0:
+            if np.min(self.f)<0.0<np.max(self.f):
+                fshift = 0.0
+            else:
+                fshift = np.average(self.f)
+        else:
+            fshift = np.average(self.f[ u[-1].imin:u[0].imax ] )
+
+        dove = 0
+        beg = self.f[0]<fshift
+        for i in range(len(self.z)):
+            now = self.f[i]<fshift
+            if now is not beg:
+                dove = i
+                break
+        if indexonly is True:
+            return dove
+        zshift = self.z[dove]
+        #print(dove,zshift,fshift)
+        return zshift,fshift
+
+    def hasFished(self,forceThreshold,toJump=False,detachOnly=False):
+        if detachOnly is True:
+            if self.getDetachForce()>forceThreshold:
+                return True
+            return False 
+        else:
+            y0 = self.getOrigin()[1]
+            if toJump is True:
+                y1 = self.getNearFarPoint()[1]
+            else:
+                y1 = self.getJumpPoint()[1]
+            if (np.abs(y1-y0)>forceThreshold):
+                return True
+            return False
+
+    def getUltimi(self):
+        if len(self.traits) <2:
+            return []
+        ultimi = []
+        for t in self.traits:
+            if t.accept is True:
+                ultimi.append(t)
+                if t.last is True:
+                    return ultimi
+        return []
+
+    def getUltimiPenultimi(self):
+        if len(self.traits) <2:
+            return [],[]
+        ultimi = []
+        penultimi = []
+        ultimo = True
+        for t in self.traits:
+            if ultimo is True:
+                if t.accept is True:
+                    ultimi.append(t)
+                    if t.last is True:
+                        ultimo = False
+            else:
+                if t.accept is True:
+                    penultimi.append(t)
+                    if t.last is True:
+                        return ultimi,penultimi
+        return [],[]
+
+    def getJumpSlope(self):
+        p = self.getUltimiPenultimi()[1]
+        if len(p)==0:
+            return 0
+        return p[-1].slope(angle=False)
+        
+    def getNearFarPoint(self,indexonly=False):
+        if len(self.traits) <2:
+            i = np.argmax(self.f)
+            if indexonly is True:
+                return i
+            return [self.z[i],self.f[i]]
+        for t in self.traits:
+            if t.accept is True and t.pj=='J':
+                i = np.argmax(t.x)
+                if indexonly is True:
+                    return t.imin+i
+                return t.x[i],t.y[i]
+        i = np.argmax(self.f)
+        if indexonly is True:
+            return i
+        return [self.z[i],self.f[i]]
+
+    def getJumpPoint(self,indexonly=False):
+        u,p = self.getUltimiPenultimi()
+        if len(u)==0 or len(p)==0:
+            if indexonly is True:
+                return np.argmax(self.f)
+            return self.z[np.argmax(self.f)],np.max(self.f)
+        x = self.z[p[0].imax:u[-1].imin]
+        y = self.f[p[0].imax:u[-1].imin]
+        if indexonly is True:
+            return p[0].imax+np.argmax(y)
+        return x[np.argmax(y)],np.max(y)
+
+    def getDetachPoint(self):
+        return self.getJumpPoint()[0]
+
+    def getDetachForce(self):
+        return self.getJumpPoint()[1]
 
     def getAdhesion(self):
         return np.max(self.f)
 
+
 if __name__ == "__main__":
-    print ('not for direct use')
+    print('not for direct use')
